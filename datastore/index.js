82fc7a19c425818ef2e0c4c97ff7f75189f660f6
Hannah Manfredi <hmanfredi@gmail.com>
2021-02-24 18:42:11 -0800

    "Complete retrieving one todo"

--- datastore/index.js
+++ datastore/index.js
@@ -41,13 +41,23 @@ exports.readAll = (callback) => {
 
 };
 
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id, text });
+  // }
+
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  const route = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(route, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, {id: id, text: data.toString()});
+    }
+  });
+
 };
 
 exports.update = (id, text, callback) => {
@@ -81,18 +91,9 @@ exports.initialize = () => {
   }
 };
 
-// Retrieving all Todos
-// Next, refactor the readAll function by returning an array of todos to client app whenever a
-//GET request to the collection route occurs. To do this, you will need to read the dataDir directory
-//and build a list of files. Remember, the id of each todo item is encoded in its filename.
+// Retrieving one Todo
+// Next, refactor the readOne to read a todo item from the dataDir based on the message's id. For this function, you must read the contents of the todo item file and respond with it to the client.
 
-// VERY IMPORTANT: at this point in the basic requirements, do not attempt to read the contents of
-//each file that contains the todo item text. Failing to heed this instruction has the potential to send you down
-//a rabbit hole.
+// You'll know you've correctly written this function because when you click edit on the UI, you'll see the todo text in the popup window.
 
-// Please note, however, you must still include a text field in your response to the client, and it's
-//recommended that you use the message's id (that you identified from the filename) for both the id field and the
-//text field. Doing so will have the effect of changing the presentation of your todo items for the time being;
-//we'll address this issue shortly.
-
-// Commit your progress: "Complete retrieving all todos"
+// Commit your progress: "Complete retrieving one todo"
