a4691f94945785defacd6f602fbc741d0f525b97
Hannah Manfredi <hmanfredi@gmail.com>
2021-02-27 15:54:10 -0800

    Complete Bare Minimum Requirements

--- datastore/index.js
+++ datastore/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
+Promise.promisifyAll(fs);
 
 var items = {};
 
@@ -23,23 +25,105 @@ exports.create = (text, callback) => {
 
 };
 
+// exports.readAll = (callback) => {
+
+//   fs.readdir(exports.dataDir, (err, data) => {
+//     if (err) {
+//       console.log('does not exist');
+//     } else {
+//       const todos = [];
+//       data.forEach(item => {
+//         let newId = item.slice(0, -4);
+//         let todo = {id: newId, text: newId};
+//         todos.push(todo);
+//       });
+//       callback(null, todos);
+//     }
+//   });
+
+// };
+
+// exports.readAll = (callback) => {
+//   let todos = [];
+//   fs.readdir(exports.dataDir, (err, data) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, todos)
+//     }
+//   });
+//   console.log('files: ', files);
+//   if (files.length === 0) {
+//     return todos;
+//   } else {
+//     const todo = {};
+//     files.forEach(item => {
+//       let newId = item.slice(0, -4).toString();
+//       todo.id = newId;
+//     });
+//   }
+// };
+
+// exports.readAll = (callback) => {
+//   const todos = [];
+//   let files = fs.readdir(exports.dataDir);
+//   console.log('files: ', files);
+//   if (files.length === 0) {
+//     return todos;
+//   } else {
+//     files.forEach(item => {
+//       let todo = {};
+//       let newId = item.slice(0, -4).toString();
+//       todo.id = newId;
+//       exports.readOne(newId, (err, data) => {
+//         if (err) {
+//           callback(err, 0);
+//         } else {
+//           callback(null, data);
+//         }
+//       })
+//       todos.push(todo);
+//     });
+//   }
+//   return Promise.all(todos);
+// };
+
 exports.readAll = (callback) => {
 
-  fs.readdir(exports.dataDir, (err, data) => {
+  const todos = [];
+  fs.readdir(exports.dataDir, (err, files) => {
     if (err) {
-      console.log('does not exist');
+      callback(err, 0);
     } else {
-      const todos = [];
-      data.forEach(item => {
-        let newId = item.slice(0, -4);
-        let todo = {id: newId, text: newId};
-        todos.push(todo);
-      });
-      callback(null, todos);
-    }
-  });
+      files.forEach(item => {
+        const todo = {};
+        let newId = item.slice(0, -4).toString();
+        todo.id = newId;
+        let filepath = path.join(exports.dataDir, item);
+        return fs.readFileAsync(filepath)
+          .then((data) => {
+            if (!todo.text) {
+              todo.text = data.toString();
+            }
+            todos.push(todo);
+            return todo;
+          })
+          .catch((err) => {
+            return err;
+          });
+        });
+        setTimeout(() => {
 
-};
+          Promise.all(todos)
+            .then((data) => {
+              callback(null, data);
+            })
+            .catch((err) => {
+            })
+        }, 25,000);
+        }
+      });
+    };
 
 exports.readOne = (id, callback) => {
   const route = path.join(exports.dataDir, `${id}.txt`);
@@ -50,7 +134,6 @@ exports.readOne = (id, callback) => {
       callback(null, {id: id, text: data.toString()});
     }
   });
-
 };
 
 exports.update = (id, text, callback) => {
@@ -74,7 +157,7 @@ exports.update = (id, text, callback) => {
         }
       });
     }
-  })
+  });
 
 };
 
@@ -91,7 +174,7 @@ exports.delete = (id, callback) => {
         } else {
           callback();
         }
-      })
+      });
     }
   });
 
@@ -107,9 +190,9 @@ exports.initialize = () => {
   }
 };
 
-// Deleting a Todo
-// Lastly, refactor the delete function to remove the todo file stored in the dataDir based on the supplied id.
+// Finish fixing readAll
+// At this point, it's time to circle back to finishing your work on readAll. You should first find the test for readAll and refactor it to expect the correct todo text instead of the id. Next, you'll need to refactor the function. Because each todo entry is stored in its own file, you'll end up with many async operations (n files = n async operations) that all need to complete before you can respond to the API request. This poses a significant challenge: your next task is to read up on promises to see how they can help you. (Hint, you'll very likely need to make use of Promise.all.)
 
-// You'll know this is working because when you refresh the page, the delete todo item will no longer be present.
+// Learn about promises by completing the 'Bare Miniumum Requirements' of [Course] Promises. Then come back to this course and complete readAll.
 
-// Commit your progress: "Complete deleting a todo"
+// Commit your progress: "Complete Bare Minimum Requirements"
